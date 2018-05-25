7000e34dacadee17544f7769e8c3bbb692727107
astur <astur@yandex.ru>
2018-05-25 19:54:35 +0300

    first tests

--- test.js
+++ test.js
@@ -2,6 +2,17 @@ const test = require('ava');
 const m = require('.');
 
 test('errsome', t => {
-    t.true(true);
-    t.is(m, m);
+    t.is(typeof m, 'function');
+});
+
+test('main', t => {
+    const err = new Error('TEST');
+    err.field = 'bla';
+    const mErr = m(err);
+    t.is(Object.keys(mErr).length, 4);
+    t.deepEqual(Object.keys(mErr), ['name', 'message', 'stack', 'field']);
+    t.true(Array.isArray(mErr.stack));
+    t.is(mErr.field, 'bla');
+    t.is(mErr.message, 'TEST');
+    t.is(mErr.name, 'Error');
 });
