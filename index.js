91f74b50e18009493668ba8241f62fdc6f93acfb
astur <astur@yandex.ru>
2018-05-25 19:55:41 +0300

    first code

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-module.exports = () => {};
+const errsome = err => Object.assign({
+    name: err.name,
+    message: err.message,
+    stack: err.stack && err.stack.split ? err.stack.split('\n').map(s => s.trim()) : err.stack,
+}, err);
+
+module.exports = errsome;
